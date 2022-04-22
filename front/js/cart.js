0dadb9f67b415e9af50e5759be8347acf53485ba
zaynakaadan <zk10-91@hotmail.com>
2022-04-22 12:32:24 +0200

    removeProduct

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,8 @@
 // ------Récupération des donées du local storage------
+let someProduct = [];
 
 let addProduit = JSON.parse(localStorage.getItem('cartItems'));
+
 console.log(addProduit);
 const cartDisplay = async () => {   //--j'ai construire mon fonction qui vas tout le code à l'intérieur*/
    //----------------Affichage des éléments dans le code HTML-----------------
@@ -63,6 +65,7 @@ let productItemContentSettingsQuantity = document.createElement("div");
 productItemContentSettings.appendChild(productItemContentSettingsQuantity);
 productItemContentSettingsQuantity.className = "cart__item__content__settings__quantity";
 
+
 // Insertion de "Qté : "
 let productQte = document.createElement("p");
 productItemContentSettingsQuantity.appendChild(productQte);
@@ -74,9 +77,12 @@ productItemContentSettingsQuantity.appendChild(productQuantity);
 productQuantity.value = addProduit[product].quantite;
 productQuantity.className = "itemQuantity";
 productQuantity.setAttribute("type", "number");
-productQuantity.setAttribute("min", "1");
-productQuantity.setAttribute("max", "100");
+productQuantity.setAttribute("min", 1);
+productQuantity.setAttribute("max", 100);
 productQuantity.setAttribute("name", "itemQuantity");
+productQuantity.setAttribute("data-id",addProduit[product]._id);
+productQuantity.setAttribute("data-color",addProduit[product].colors);
+
 
 // Insertion de l'élément "div"
 let productItemContentSettingsDelete = document.createElement("div");
@@ -89,6 +95,8 @@ let productSupprimer = document.createElement("p");
 productItemContentSettingsDelete.appendChild(productSupprimer);
 productSupprimer.className = "deleteItem";
 productSupprimer.innerHTML = "Supprimer";
+productSupprimer.setAttribute("data-id",addProduit[product]._id);
+productSupprimer.setAttribute("data-color",addProduit[product].colors);
 productSupprimer.addEventListener("click", () => {
     console.log("vous avez supprimé le item");
 }
@@ -96,10 +104,6 @@ productSupprimer.addEventListener("click", () => {
 }
 }
     cartDisplay();
-
-
-
-
 
 
 
@@ -119,15 +123,16 @@ console.log('totalPrice', totalPrice)
         
 }
 getTotals();
-// ajout un plus de la quantité depuis la page panier
+
+// ajoute un plus de la quantité depuis la page panier
 const plusQuantite = async (cartDisplay) => {
     await cartDisplay;
     console.log("fonction plus");
-    let plus = document.querySelectorAll(".cart__item" )
+    let plus = document.querySelectorAll(".itemQuantity") 
     console.log(plus);
     plus.forEach((positive) => {
         
-        positive.addEventListener("click" , () => {
+        positive.addEventListener("click" , () =>  {
             console.log(positive);
 
             for(i=0; i< addProduit.length;i++){
@@ -152,14 +157,95 @@ plusQuantite();
 
 
 
+const minQuantite = async (cartDisplay) => {
+  await cartDisplay;
+  let moins = document.querySelectorAll(".itemQuantity");
+  moins.forEach((negative) => {
+    negative.addEventListener("click" , ()  => {
+      console.log(negative);
 
 
+    let totalAddProduit = addProduit.length;
+    for(i=0 ; i<totalAddProduit; i++){
+    console.log(totalAddProduit);
+   
+    if(addProduit[i].quantite == 1 && totalAddProduit == 1){
+        return (
+         localStorage.removeItem('cartItems'),
+        (location.href = "cart.html") ,
+        console.log("remove moins tout")
+        );   
+    }
+    if(addProduit[i].quantite == 1 && totalAddProduit != 1 &&      //un condition pour 1 produit et plusieur produits
+        addProduit[i]._id == negative.dataset.id && addProduit[i].colors == negative.dataset.color ){///il y a plusieur produits dans le tableau mais il y en a un qui a une 1 quantité il faut suprimer cette produit la
+        addProduit.splice(i,1)    //pour supprimer un 1 élément à cet index
+        
+        localStorage.setItem('cartItems', JSON.stringify(addProduit));
+//Refresh rapide de la page
+        location.href = "cart.html";
+        console.log("moins un produit dans le panier ");
+    }
+    if(
+        addProduit[i].quantite != 1 && totalAddProduit != 1 &&
+        addProduit[i]._id == negative.dataset.id && addProduit[i].colors == negative.dataset.color){
+
+        return (
+            addProduit[i].quantite--,
+            localStorage.setItem("cartItems",JSON.stringify(addProduit),
+            
+            console.log("quantite--"))
+            );
+    }
+}
+
+      
+    })  
+    })
+};
+
+minQuantite();
 
 
+const removeProduct = async (cartDisplay) => {
+        await cartDisplay
+        
+    console.log("remove product");
+    let corbeilles = document.querySelectorAll(".deleteItem")
+    console.log(corbeilles);
+    corbeilles.forEach((corbeille) => {corbeille.addEventListener("click", ()=> {
+console.log(corbeille);
 
+let totalAddProduitRemove = addProduit.length;
+console.log(totalAddProduitRemove);
+alert("Votre article est bien supprimé")
+if(totalAddProduitRemove ==1) {//si il y a 1 produit
+    return (localStorage.removeItem('cartItems'),
+    
+    console.log("remove tout le panier")
+    )
+}
+else {
+someProduct = addProduit.filter(el => {
+    if(corbeille.dataset.id != el._id || corbeille.dataset.color !=el.colors){
+        return true;
+       
+    }
+    
+   }
+    )
+    console.log(someProduct);
+    localStorage.setItem('cartItems',JSON.stringify(someProduct));
+    
+    location.href = "cart.html";
+    console.log(" corbeille remove le produit en question");
+    
+}
+    });
+});
+return;
+};
 
-
-
+removeProduct();
 
 
 
